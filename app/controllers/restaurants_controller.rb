97777c93388be964298e95700a9ac20c55c65ce3
kennytrionfo <kentrionfo@gmail.com>
2015-04-23 19:21:55 -0600

    rest edit file

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+	before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@restaurants = Restaurant.all.order("created_at DESC")
@@ -17,8 +18,10 @@ class RestaurantsController < ApplicationController
 		end
 	end
 
+	def show
+	end
+
 	def edit
-	
 	end
 	
 	def update
@@ -40,6 +43,10 @@ class RestaurantsController < ApplicationController
 
 	def restaurant_params
 		params.require(:restaurant).permit(:name, :description, :address)
+	end
+
+	def find_restaurant
+		@restaurant = Restaurant.find(params[:id])
 	end
 
 end
