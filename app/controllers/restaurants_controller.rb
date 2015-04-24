ff8a62aad6145adeb4793b85731d6402856994dd
kennytrionfo <kentrionfo@gmail.com>
2015-04-23 18:06:56 -0600

    new and create in controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantsController < ApplicationController
+
+	def index
+	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		@restaurant = Restaurant.new(restaurant_params)
+	end
+
+
+
+
+	private
+
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :description, :address)
+	end
+
 end
