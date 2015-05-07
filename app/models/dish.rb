38caa53900a45de1ab87c82141e525daa8ec3243
kennytrionfo <kentrionfo@gmail.com>
2015-05-06 20:14:30 -0600

    added paperclip gem to dishes model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,7 @@
 class Dish < ActiveRecord::Base
 	belongs_to :restaurant
+
+	has_attached_file :image, styles: { :medium => "300x300>" } #paperclip gem stuff
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ #paperclip gem stuff
+
 end
